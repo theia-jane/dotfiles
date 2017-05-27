8978d4277a12253bbe313c485c659376540e19d7
Theia Ware <theia.j.ware@gmail.com>
2017-05-27 15:06:50 -0600

    intial commit of dot files with script to unpack them :)

---
+++ setup-dot-files.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+for dot_file in $(git ls-files | grep '^.'); do
+
+file_dest="${HOME}/${dot_file}" 
+if [ -f "${file_dest}" ]; then
+  echo "${file_dest}: File exists, making a backup."
+  mv "${file_dest}" "${file_dest}.bk"
+fi
+
+echo "${dot_file}: Linking file"
+ln -s "${dot_file}" "${file_dest}"
+
+done
+
+
