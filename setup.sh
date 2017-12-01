becbd520e9393c14e3bccc9dfdaabb24ff6c4630
Theia Ware <theia.j.ware@gmail.com>
2017-12-01 12:03:48 -0700

    fixing setup and moving gitconfig to sym link

--- setup.sh
+++ setup.sh
@@ -1,12 +1,16 @@
 #!/bin/bash
 
-dot_dir='./home'
+dot_dir='home'
 for dot_file in $(cd $dot_dir && ls -1); do
-echo "${dot_file}: Linking file"
+  echo "${dot_file}: Linking file"
 
-file_dest="${HOME}/.${dot_file}" 
-rm "${file_dest}"
-ln "${dot_dir}/${dot_file}" "${file_dest}"
+  symbolic=""
+  if [[ "$dot_file" = 'gitconfig' ]]; then
+    symbolic="-s"
+  fi
+  file_dest="${HOME}/.${dot_file}" 
+  rm "${file_dest}"
+  ln $symbolic "$(pwd)/${dot_dir}/${dot_file}" "${file_dest}"
 done
 
 
