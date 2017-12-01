92aea5e06aaf551fbafe712c95560fc854e5a882
Theia Ware <theia.j.ware@gmail.com>
2017-12-01 11:10:05 -0700

    restructuring for future development and remote configs

--- setup.sh
+++ setup.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 
-for dot_file in $(git ls-files | grep '^\.'); do
+dot_dir='./home'
+for dot_file in $(cd $dot_dir && ls -1); do
 echo "${dot_file}: Linking file"
 
-file_dest="${HOME}/${dot_file}" 
+file_dest="${HOME}/.${dot_file}" 
 rm "${file_dest}"
-ln "${dot_file}" "${file_dest}"
+ln "${dot_dir}/${dot_file}" "${file_dest}"
 done
 
 
