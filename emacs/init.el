8b762273044991bf3fddb2daa2d961051a699be5
Theia Ware <theia.j.ware@gmail.com>
2018-03-09 16:13:45 -0700

    adding paredit, changing default settings with ivy and adding niceness for org todo

--- emacs/init.el
+++ emacs/init.el
@@ -29,7 +29,7 @@
 (require 'package)
 (setq package-enable-at-startup nil)
 (setq package-archives '(("org" . "http://orgmode.org/elpa/")
-			 ("gnu" . "https:/elpa.gnu.org/packages/")
+			 ("gnu" . "https://elpa.gnu.org/packages/")
 			 ("melpa" . "https://melpa.org/packages/")
 			 ("marmalade" . "https://marmalade-repo.org/packages/")))
 
@@ -41,8 +41,23 @@
 
 
 (use-package general :ensure t)
+(use-package paredit
+  :ensure t
+  :diminish paredit-mode
+  :config
+  (add-hook 'emacs-lisp-mode-hook       #'enable-paredit-mode)
+  (add-hook 'eval-expression-minibuffer-setup-hook #'enable-paredit-mode)
+  (add-hook 'ielm-mode-hook             #'enable-paredit-mode)
+  (add-hook 'lisp-mode-hook             #'enable-paredit-mode)
+  (add-hook 'lisp-interaction-mode-hook #'enable-paredit-mode)
+  (add-hook 'scheme-mode-hook           #'enable-paredit-mode))
 
-;; Only the evilest
+(use-package evil-paredit :ensure t
+  :config
+  (add-hook 'emacs-lisp-mode-hook 'evil-paredit-mode))
+(use-package restclient :ensure t)
+
+;; only the evilest
 (use-package evil :ensure t
 	     :config
 	     (evil-mode 1)
@@ -86,13 +101,42 @@
 (use-package ivy :ensure t
   :config
   ;; clears the intial ^ when using ivy
-  (setq ivy-initial-inputs-alist nil))
+  (setq ivy-initial-inputs-alist nil)
+  :bind*
+  (("C-x b" . ivy-switch-buffer)
+   ("C-x C-b" . ivy-switch-buffer))
+  :bind (:map ivy-minibuffer-map
+	      ("C-n" . ivy-next-history-element)
+	      ("C-p" . ivy-previous-history-element)
+	      ("C-k" . ivy-previous-line)
+	      ("C-j" . ivy-next-line)
+	      ))
 
 (use-package counsel :ensure t
   :bind*
   (("M-x" . counsel-M-x)
    ("C-x f" . counsel-find-file)))
 
+(use-package org
+  :ensure t
+  :config
+  (font-lock-add-keywords            
+   'org-mode `(("^\\*+ \\(TODO\\) "  
+                (1 (progn (compose-region (match-beginning 1) (match-end 1) "⚑")
+                          nil)))
+               ("^\\*+ \\(DOING\\) "
+                (1 (progn (compose-region (match-beginning 1) (match-end 1) "⚐")
+                          nil)))
+               ("^\\*+ \\(CANCELED\\) "
+                (1 (progn (compose-region (match-beginning 1) (match-end 1) "✘")
+                          nil)))
+               ("^\\*+ \\(DONE\\) "
+                (1 (progn (compose-region (match-beginning 1) (match-end 1) "✔")
+                          nil)))))
+  )
+
+;(general )
+;~/.config/personal/dotfiles/emacs/init.el
 
 ;; Example of loading & parsing some JSON
 ;; https://emacs.stackexchange.com/questions/27407/accessing-json-data-in-elisp
