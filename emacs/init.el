df7880e2075d76efb4727c53fc9ef0c5000c1ba5
Theia Ware <theia.j.ware@gmail.com>
2018-03-12 07:52:13 -0600

    getting more usable bindings and setting up different packages

--- emacs/init.el
+++ emacs/init.el
@@ -1,26 +1,14 @@
 ;; OS config tools
 
-; (defun get-system-type () system-type)
-; (defun is-os (os-name) (eq (get-system-type) os-name))
-; (defun is-mac () (is-os 'darwin))
-; (defun is-linux() (is-os 'gnu/linux))
-;
-; (defun os-switch (mac linux)
-;   (cond ((is-mac) mac)
-;         ((is-linux) linux)))
-;
-;
 ; ;; Swapping
 ; (setq use-spacemacs nil)   ; or nil 
 ; (when use-spacemacs
 ;     (setq user-emacs-directory "~/.spacemacs.d/"))   ; defaults to ~/.emacs.d/
 ; (load (expand-file-name "init.el" user-emacs-directory))
 
-
 (defun is-system (system-name) (eq system-type system-name))
 (defun is-mac () (is-system 'darwin))
 (defun is-linux () (is-system 'gnu/linux))
-
 (defun system-cond ()
   (cond ((is-mac) mac)
 	((is-linux) linux)))
@@ -39,18 +27,54 @@
   (package-refresh-contents)
   (package-install 'use-package))
 
+;; Key bindings!
+(use-package general :ensure t
+  :config
+  (general-define-key
+   :states '(normal visual insert emacs)
+   :prefix "SPC"
+   :non-normal-prefix "C-SPC"
 
-(use-package general :ensure t)
+   "SPC"   'counsel-M-x
+   "f d e" '(lambda () (interactive) (find-file "~/.config/personal/dotfiles/emacs/init.el"))
+   "f d f" '(lambda () (interactive) (counsel-find-file "~/.config/personal/dotfiles"))
+   "f o f" '(lambda () (interactive) (counsel-find-file "~/org/"))
+   "f f" 'counsel-find-file
+
+   "b b" 'ivy-switch-buffer
+   ))
+
+;; Workspaces
+(use-package perspective
+  :ensure t
+  :config
+  (persp-mode t))
+
+(use-package powerline :ensure t)
+(use-package powerline-evil :ensure t
+  :config
+  (powerline-evil-center-color-theme))
+
+;; Shut up the bell
+(defun my-bell-function ())
+(setq ring-bell-function 'my-bell-function)
+(setq visible-bell nil)
+
 (use-package paredit
   :ensure t
   :diminish paredit-mode
   :config
-  (add-hook 'emacs-lisp-mode-hook       #'enable-paredit-mode)
-  (add-hook 'eval-expression-minibuffer-setup-hook #'enable-paredit-mode)
-  (add-hook 'ielm-mode-hook             #'enable-paredit-mode)
-  (add-hook 'lisp-mode-hook             #'enable-paredit-mode)
-  (add-hook 'lisp-interaction-mode-hook #'enable-paredit-mode)
-  (add-hook 'scheme-mode-hook           #'enable-paredit-mode))
+  (dolist
+      (mode-hook '(emacs-lisp-mode-hook
+		   eval-expression-minibuffer-setup-hook
+		   ielm-mode-hook
+		   lisp-mode-hook
+		   lisp-interaction-mode-hook
+		   scheme-mode-hook))
+    (add-hook mode-hook #'enable-paredit-mode)))
+	 
+
+
 
 (use-package evil-paredit :ensure t
   :config
@@ -60,10 +84,10 @@
 ;; only the evilest
 (use-package evil :ensure t
 	     :config
-	     (evil-mode 1)
-	     )
+	     (evil-mode 1))
 
-(use-package evil-org :ensure t)
+;; (use-package evil-org :ensure t)
+
 (use-package which-key :ensure t
   :config
   (which-key-mode 1))
@@ -81,16 +105,10 @@
 
 ;; Set font niceness
 (set-default-font "Source Code Pro-14")
-(use-package spacegray-theme :ensure t
-  :config
-  (load-theme 'spacegray t))
 
-(use-package org-bullets :ensure t
+(use-package darktooth-theme :ensure t
   :config
-  (add-hook 'org-mode-hook (lambda () (org-bullets-mode 1)))
-  )
-
-;; (use-package org-beautify-theme :ensure t)
+  (load-theme 'darktooth t))
 
 ;; Example of telling when in terminal (need to still figure out emacs client)
 ;; https://emacs.stackexchange.com/questions/13050/different-theme-for-nw-terminal
@@ -102,6 +120,9 @@
   :config
   ;; clears the intial ^ when using ivy
   (setq ivy-initial-inputs-alist nil)
+  (setq ivy-re-builders-alist
+      '((ivy-switch-buffer . ivy--regex-plus)
+        (t . ivy--regex-fuzzy)))
   :bind*
   (("C-x b" . ivy-switch-buffer)
    ("C-x C-b" . ivy-switch-buffer))
@@ -113,28 +134,47 @@
 	      ))
 
 (use-package counsel :ensure t
-  :bind*
+  :bind
   (("M-x" . counsel-M-x)
    ("C-x f" . counsel-find-file)))
+
+;; Fuzzy finding for emacs
+(use-package flx :ensure t)
+
+;; mapping an associative list
+(defun map-alist (f alist)
+    (mapcar (lambda (key-val)
+	    (setq key (car key-val)
+		    val (cdr key-val))
+	    (funcall f key val))
+	    alist))
+
+;; Map keywords (TODO) to a nicer icon 
+(defun org-mode-todo-symbols (todo-alist)
+  (setq org-todo-font-lock-replace
+	(map-alist (lambda (keyword symbol)
+		 `(,(concat "^\\*+ \\(" keyword "\\) ") 
+		   (1 (progn (compose-region (match-beginning 1) (match-end 1) ,symbol) nil))))
+	       todo-alist))
+
+  (font-lock-add-keywords            
+   'org-mode org-todo-font-lock-replace))
+
 
 (use-package org
   :ensure t
   :config
-  (font-lock-add-keywords            
-   'org-mode `(("^\\*+ \\(TODO\\) "  
-                (1 (progn (compose-region (match-beginning 1) (match-end 1) "⚑")
-                          nil)))
-               ("^\\*+ \\(DOING\\) "
-                (1 (progn (compose-region (match-beginning 1) (match-end 1) "⚐")
-                          nil)))
-               ("^\\*+ \\(CANCELED\\) "
-                (1 (progn (compose-region (match-beginning 1) (match-end 1) "✘")
-                          nil)))
-               ("^\\*+ \\(DONE\\) "
-                (1 (progn (compose-region (match-beginning 1) (match-end 1) "✔")
-                          nil)))))
-  )
+  (org-mode-todo-symbols
+   '(("TODO" . "⚑")
+     ("DOING" .  "⚐")
+     ("CANCELED" .  "✘")
+     ("DONE" .  "✔"))))
 
+;; Org niceness
+(use-package org-bullets :ensure t
+  :init (add-hook 'org-mode-hook 'org-bullets-mode))
+(use-package worf :ensure t
+  :init (add-hook 'org-mode-hook 'worf-mode))
 ;(general )
 ;~/.config/personal/dotfiles/emacs/init.el
 
