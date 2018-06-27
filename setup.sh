0dc8ab0e6fddcd92bf3ebce7be3ba4b8946d3126
Theia Ware <theia.j.ware@gmail.com>
2018-06-27 16:59:29 -0600

    Fixing tmux issues with panes and adding scheme for node tools. Adding mermaid.cli

--- setup.sh
+++ setup.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
+config_msg() {
+  echo "Configuring: $@"
+  echo '------------------------------------------------------------'
+}
+
+config_msg 'Home'
+
 dot_dir='home'
 for dot_file in $(cd $dot_dir && ls -1); do
   echo "${dot_file}: Linking file"
@@ -13,4 +20,13 @@ for dot_file in $(cd $dot_dir && ls -1); do
   ln $symbolic "$(pwd)/${dot_dir}/${dot_file}" "${file_dest}"
 done
 
+
+config_msg 'Node Tools'
+
+node_tools="node_tools"
+(
+  cd $node_tools; \
+  yarn install; \
+)
+ln -s "$(pwd)/${node_tools}" "${HOME}/${node_tools}"
 
