a1087b9fdedb266297aa0444db8cdbb85a19eb82
Theia Ware <theia.j.ware@gmail.com>
2019-11-08 09:58:19 -0700

    Changing how script selection happens in cat.s

--- src/scripts/cat.s
+++ src/scripts/cat.s
@@ -1,10 +1,6 @@
 #!/bin/bash
 
-SCRIPT_NAME="$1"
-
-if [ -z "$SCRIPT_NAME" ]; then
-  SCRIPT_NAME="$(cd $s && ls | $(fuzzy-ui-provider))"
-fi
+SCRIPT_NAME="$(sel -d "$1" script-list)"
 
 if [ ! -z "$SCRIPT_NAME" ]; then
   bat "$(which "$SCRIPT_NAME")"
