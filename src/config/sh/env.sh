a837cdb9a8e7f226a37f108f5b7de2f0ac8d767e
Theia Ware <theia.j.ware@gmail.com>
2019-11-18 13:41:20 -0700

    Adding doom env var and changing up EDITOR
    Editor will default to emacsdclient, then nvim and then vim

--- src/config/sh/env.sh
+++ src/config/sh/env.sh
@@ -3,15 +3,23 @@ export cfg_sh="${cfg}/sh"
 export docs="${HOME}/Documents"
 export dbox="${HOME}/Nextcloud"
 export org="${HOME}/org"
+
 export p="${HOME}/Projects"
 export dot="$p/dotfiles"
+export DOOMDIR="${cfg}/doom"
 export scripts="$dot/src/scripts"
 export s=$scripts
 export CLICOLOR=1
 export GTAGSLABEL="pygments"
 
 
+if [ "$(which emacsdclient)" ]; then
+  EDITOR="emacsdclient -c"
+fi
+
 if [ "$(which nvim)" ]; then
-  export EDITOR=nvim
+  EDITOR="${EDITOR:-nvim}"
   export MANPAGER="nvim -c 'set ft=man' -"
 fi
+
+export EDITOR="${EDITOR:-vim}"
