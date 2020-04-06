77f1914ad70d758908dd495a84a71d7ea4d366ba
Theia Ware <theia.j.ware@gmail.com>
2020-04-05 21:39:04 -0600

    Handling errors in '+elisp/eval-outer-sexp

--- src/config/doom/+elisp.el
+++ src/config/doom/+elisp.el
@@ -1,15 +1,17 @@
 ;;; ~/Projects/dotfiles/src/config/doom/+elisp.el -*- lexical-binding: t; -*-
 
-(defun +elisp/eval-outer-sexp ()
-  (interactive)
+(defun +elisp/eval-outer-sexp (eval-last-sexp-arg-internal)
+  "Evaluate outermost sexp, this utilizes the 'eros-eval-last-sexp"
+  (interactive "P")
   (save-excursion
     (let* ((ppss (syntax-ppss))
            (outer-sexp-posns (nth 9 ppss)))
       (when outer-sexp-posns
         (goto-char (car outer-sexp-posns))))
-    (forward-char)
-    (sp-end-of-sexp)
-    (eros-eval-last-sexp nil)))
+    (if (not (eq nil (sexp-at-point)))
+        (progn (sp-end-of-next-sexp)
+               (eros-eval-last-sexp eval-last-sexp-arg-internal))
+      (message "Not inside sexp"))))
 
 
 (general-define-key
