511ca58457b160c6b38b66154bf323828c0895b9
Theia Ware <theia.j.ware@gmail.com>
2020-04-07 16:04:39 -0600

    Adding snippet keybindings

--- src/config/doom/+key.el
+++ src/config/doom/+key.el
@@ -9,7 +9,16 @@
                                           (find-file file)))))
  ;; Leader bindings
  (:leader
-   :desc "Toggle Highlight" "t h" 'evil-ex-nohighlight)
+   :desc "Toggle Highlight" "t h" 'evil-ex-nohighlight
+   (:prefix ("S" . "snippets")
+     :desc "New snippet"          "n" #'+snippets/new
+     :desc "Edit snippet"         "e" #'+snippets/edit
+     :desc "Make alias snippet"   "a" #'+snippets/new-alias
+     (:prefix ("f" . "find")
+       :desc "Find"               "f" #'+snippets/find-for-current-mode
+       :desc "Find mine"          "m" #'+snippets/find-private
+       :desc "Find all"           "a" #'+snippets/find
+       )))
  ;; Global bindings
  (:desc "Jump forward" "C->" 'better-jumper-jump-forward
    :desc "Jump back" "C-<" 'better-jumper-jump-backward))
