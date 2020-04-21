561d5ea95b23e6d228a2ced23feed5abdb7ceef0
Theia Ware <theia.j.ware@gmail.com>
2020-04-21 09:18:05 -0600

    Refactoring sexp and elisp sexp eval code

--- src/config/doom/+elisp.el
+++ src/config/doom/+elisp.el
@@ -1,50 +1,56 @@
 ;;; ~/Projects/dotfiles/src/config/doom/+elisp.el -*- lexical-binding: t; -*-
 
+(defun beginning-of-sexp-p ()
+  "Test if the point is at the beginning of a sexp."
+  (= (point)
+     (save-excursion
+       (end-of-sexp)
+       (beginning-of-sexp)
+       (point))))
 
-(defun +elisp/goto-start-of-outer-sexp ()
+(defun end-of-sexp-p ()
+  "Test if the point is at the end of a sexp."
+  (= (point)
+     (save-excursion
+       (beginning-of-sexp)
+       (end-of-sexp)
+       (point))))
+
+(defun outermost-list (&optional escape-strings no-syntax-crossing)
+  "Move point to the end of the outermost list relative to the current point."
+  (interactive "d\nd")
+  ;; Don't ignore scan-error if we start at the wrong spot
+  (up-list 1 escape-strings no-syntax-crossing)
+  (ignore-error scan-error
+    (loop (up-list 1 escape-strings no-syntax-crossing))))
+
+(defun end-of-outermost-sexp ()
   (interactive)
-  (with-syntax-table emacs-lisp-mode-syntax-table
-    (let ((next t))
-      (while next
-        (setq next nil)
-        (ignore-error scan-error
-          (when (and (eolp) (not (bolp)))
-            (backward-char))
-          (backward-up-list)
-          (setq next t))))))
+  (ignore-error scan-error
+    (outermost-list))
+  (if (beginning-of-sexp-p)
+      (end-of-sexp)))
 
-(defun +elisp/goto-end-of-outer-sexp ()
-  (interactive)
-  (with-syntax-table emacs-lisp-mode-syntax-table
-    (let ((next t))
-      (while next
-        (setq next nil)
-        (ignore-error scan-error
-          (when (and (bolp) (not (eolp)))
-            (forward-char))
-          (up-list 1)
-          (setq next t))))))
-
-
-(defun +elisp/eval-outer-sexp (eval-last-sexp-arg-internal)
+(defun +elisp/eval-outermost-sexp (eval-last-sexp-arg-internal)
   "Evaluate outermost sexp, this utilizes the 'eros-eval-last-sexp"
   (interactive "P")
-  (save-excursion
-    (+elisp/goto-end-of-outer-sexp)
-    (eros-eval-last-sexp eval-last-sexp-arg-internal)))
+  (with-syntax-table emacs-lisp-mode-syntax-table
+    (save-excursion
+      (end-of-outermost-sexp)
+      (eros-eval-last-sexp eval-last-sexp-arg-internal))))
 
 
-(defun +elisp/eval-outer-sexp-then-next (eval-last-sexp-arg-internal)
-  "Evaluate outermost sexp, this utilizes the 'eros-eval-last-sexp"
+(defun +elisp/eval-outermost-sexp-and-continue (eval-last-sexp-arg-internal)
+  "Attempts to evaluate outermost sexp. If it fails, move to next sexp.
+This utilizes the 'eros-eval-last-sexp."
   (interactive "P")
-  (+elisp/goto-end-of-outer-sexp)
-  (condition-case _err
-      (eros-eval-last-sexp eval-last-sexp-arg-internal)
-    (t
-     (forward-sexp)
-     (eros-eval-last-sexp eval-last-sexp-arg-internal)))
-  (forward-sexp)
-  (+elisp/goto-start-of-outer-sexp))
+  (with-syntax-table emacs-lisp-mode-syntax-table
+    (ignore-error end-of-file
+      (+elisp/eval-outermost-sexp eval-last-sexp-arg-internal))
+    (end-of-outermost-sexp)
+    (forward-sexp)
+    (beginning-of-sexp))
+  (recenter nil t))
 
 
 ;; Key-bindings
@@ -55,8 +61,8 @@
    ;; Move 'eros-eval-last-sexp
    "C-S-e" #'eros-eval-last-sexp)
  (:map emacs-lisp-mode-map
-   :desc "Eval outermost sexp" "<S-return>" #'+elisp/eval-outer-sexp
-   "<C-S-return>" #'+elisp/eval-outer-sexp-then-next)
+   :desc "Eval outermost sexp" "<S-return>" #'+elisp/eval-outermost-sexp
+   "<C-return>" #'+elisp/eval-outermost-sexp-and-continue)
  (:localleader
    :map emacs-lisp-mode-map
    :desc "Eval outermost sexp" "e o" #'+elisp/eval-outer-sexp)
--- src/config/doom/+evil.el
+++ src/config/doom/+evil.el
@@ -13,9 +13,14 @@
   :after #'evil-ex-search
   (recenter nil t))
 
+;; FIXME -- this messes up evil-up-paren
+;;          1. Get debugging working
+;;          2. Debug through the working function, which can be 'called' by
+;;             doing =da(= on a paren on the begining of a line
+;;          3. Debug through the broken version
 (defadvice! +evil--forward-sexp--advice (fn &rest args)
   "Outside of the emulated eol with forward sexp"
-  :around 'forward-sexp
+  :around '(forward-sexp up-list)
   (when (+evil-is-emulated-eol?)
     (forward-char))
   (apply fn args))
