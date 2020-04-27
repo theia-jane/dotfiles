1d8d12fb0c8a9fd4790ee09c05ba7aa26fd4cb75
Theia Ware <theia.j.ware@gmail.com>
2020-04-26 22:24:15 -0600

    Centralized way to get a doom buffer name

--- src/config/doom/+doom.el
+++ src/config/doom/+doom.el
@@ -6,3 +6,10 @@
       doom-variable-pitch-font (font-spec :family "Source Code Pro")
       doom-unicode-font (font-spec :family "DejaVu Sans Mono")
       doom-big-font (font-spec :family "Source Code Pro" :size 48))
+
+(defun +doom-buffer-name (context)
+  (format "*doom:%s:%s*"
+          context
+          (if (bound-and-true-p persp-mode)
+              (safe-persp-name (get-current-persp))
+            "main")))
