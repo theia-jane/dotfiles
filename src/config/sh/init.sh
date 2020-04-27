d332593757d392daf5754c21f67f51aefe9594c1
Theia Ware <theia.j.ware@gmail.com>
2020-04-26 22:22:24 -0600

    Adding in function to support vterm + emacs usage

--- src/config/sh/init.sh
+++ src/config/sh/init.sh
@@ -210,6 +210,18 @@ alias .so="source '${HOME}/.${SHELL_NAME}rc'"
 alias new-script-from-last="fc -l -1 | cut -f 2- | c && new-script-from-clipboard"
 alias new-script-from-history="fc -lr 1 | cut -f 2- | fzf | c && new-script-from-clipboard"
 
+function vterm_printf(){
+    if [ -n "$TMUX" ]; then
+        # Tell tmux to pass the escape sequences through
+        # (Source: http://permalink.gmane.org/gmane.comp.terminal-emulators.tmux.user/1324)
+        printf "\ePtmux;\e\e]%s\007\e\\" "$1"
+    elif [ "${TERM%%-*}" = "screen" ]; then
+        # GNU screen (screen, screen-256color, screen-256color-bce)
+        printf "\eP\e]%s\007\e\\" "$1"
+    else
+        printf "\e]%s\e\\" "$1"
+    fi
+}
 
 #set emacs mode
 set -o emacs 
