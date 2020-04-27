4ac1a8c09ac77263643e28d27c28322847895203
Theia Ware <theia.j.ware@gmail.com>
2020-04-27 11:46:07 -0600

    Org evaluate src block in repl (eshell)

--- src/config/doom/+org.el
+++ src/config/doom/+org.el
@@ -101,11 +101,74 @@
      (org-babel-execute-src-block)))
   (+org/goto-next-src-block))
 
-(map!
-     (:map org-mode-map
+(defvar +org-babel-eval-in-repl-candidates '("bash" "sh"))
+
+(defun +org/babel-src-get-dir (header-args)
+  (let ((dir (cdr (assq :dir header-args))))
+    (if (and dir (not (file-remote-p dir)))
+        dir
+      default-directory)))
+
+(defun +org/babel-eval-in-repl ()
+  "TODO"
+  (interactive)
+  (destructuring-bind
+      (lang contents header-args &rest _)
+      (org-babel-get-src-block-info)
+    (cond ((member lang org-babel-shell-names)
+           (if (not (equal lang "eshell"))
+               (+org-babel-src-evaluate-as-script
+                lang
+                (org-babel-expand-body:generic
+                 contents header-args (org-babel-variable-assignments:shell header-args))
+                header-args))))))
+
+(defun +org-babel-src-evaluate-as-script (lang contents header-args)
+  "TODO"
+  (let* ((dir (+org/babel-src-get-dir header-args))
+         (stdin (let ((stdin (cdr (assq :stdin header-args)))
+                      (inhibit-message t))
+                  (when stdin
+                    (org-babel-sh-var-to-string (org-babel-ref-resolve stdin)))))
+         (cmdline (cdr (assq :cmdline header-args)))
+         (shebang (or
+                   (cdr (assq :shebang header-args))
+                   (format "#!/usr/bin/env %s" lang)))
+         (script-file (org-babel-temp-file (concat lang "-script-")))
+         (stdin-file (org-babel-temp-file "stdin-"))
+         (padline (not (string= "no" (cdr (assq :padline header-args))))))
+    (with-temp-file script-file
+      (insert shebang "\n")
+      (when padline (insert "\n"))
+      (insert contents))
+    (set-file-modes script-file #o755)
+    (let ((command-string
+           (cond
+            ((or stdin cmdline)
+             (with-temp-file stdin-file (insert (or stdin "")))
+             (concat "cat " stdin-file " | " script-file (and cmdline (concat " " cmdline))))
+            (t script-file))))
+      (+eshell-run-command-visibly command-string dir))))
+
+(defun +org-can-eval-in-repl? (element)
+  "TODO"
+  (let* ((type (org-element-type element))
+         (lang (org-element-property :language element)))
+    (and (eq type 'src-block)
+         (member lang +org-babel-eval-in-repl-candidates))))
+
+(defun +org/M-S-Return (arg)
+  "Change what happens depending on where the point is."
+  (interactive "P")
+  (let* ((element (org-element-at-point)))
+    (cond ((+org-can-eval-in-repl? element)
+             (+org/babel-eval-in-repl))
+          (t (org-insert-todo-heading arg)))))
+
+(map! (:map org-mode-map
      "<S-return>" 'org-ctrl-c-ctrl-c
+     "<M-S-return>" '+org/M-S-Return
      :ni "<C-S-return>" '+org/eval-src-block-then-next
      "<normal-state> <M-return>" '+tw/org-ctrl-c-ret)
      (:map evil-org-mode-map
-       :ni "<C-S-return>" '+org/eval-src-block-then-next
-       ))
+       :ni "<C-S-return>" '+org/eval-src-block-then-next))
