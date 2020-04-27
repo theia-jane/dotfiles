32fb10eaf037d99b2e76f038a538f8cfa667cb1a
Theia Ware <theia.j.ware@gmail.com>
2020-04-26 22:20:53 -0600

    Setting some defaults for doom fonts

--- src/config/doom/+doom.el
+++ src/config/doom/+doom.el
@@ -2,3 +2,7 @@
 
 
 (setq doom-scratch-initial-major-mode 'org-mode)
+(setq doom-font (font-spec :family "Source Code Pro" :size 30)
+      doom-variable-pitch-font (font-spec :family "Source Code Pro")
+      doom-unicode-font (font-spec :family "DejaVu Sans Mono")
+      doom-big-font (font-spec :family "Source Code Pro" :size 48))
