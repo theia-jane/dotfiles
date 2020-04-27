6efab41422ad7cf49fcfcf741b81bd464fb9180e
Theia Ware <theia.j.ware@gmail.com>
2020-04-26 22:21:55 -0600

    Removing extraneous settings that I don't like

--- src/config/doom/+ui.el
+++ src/config/doom/+ui.el
@@ -9,16 +9,4 @@
 (after! dired
   (add-hook 'dired-mode-hook 'dired-hide-details-mode))
 
-;; General Visual
-(defun config/visual/buffer-spacing ()
-  (setq-default
-   line-spacing 7
-   ;; header-line-format " "                                ; Add content to header line
-   left-margin-width 3
-   right-margin-width 3
-   )
-  ;; (set-face-attribute 'header-line nil :background "15") ; Set the background color of the header line to the background of the theme
-  (set-window-margins nil 2 2)                           ; Add padding at the sides
- )
-
-(config/visual/buffer-spacing)
+(setq-default line-spacing 7)
