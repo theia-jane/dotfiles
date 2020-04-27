126d2bf481f5b2e43eec450dee2c091bd08113a5
Theia Ware <theia.j.ware@gmail.com>
2020-04-26 21:42:49 -0600

    Adding core function that ensures a buffer is setup

--- src/config/doom/+core.el
+++ src/config/doom/+core.el
@@ -1,2 +1,9 @@
 ;;; ~/Projects/dotfiles/src/config/doom/+core.el -*- lexical-binding: t; -*-
 
+(defun +ensure-buffer (name &optional setup-fn)
+  (let ((buffer (get-buffer name)))
+    (unless buffer
+      (setq buffer (get-buffer-create name))
+      (with-current-buffer buffer
+          (funcall setup-fn)))
+    buffer))
