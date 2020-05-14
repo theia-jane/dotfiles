f9ba35a8880488630f8e80dba8652a53a749bc4c
Theia Ware <theia.j.ware@gmail.com>
2020-05-14 17:06:14 -0600

    Make +doom-buffer-name more general

--- src/config/doom/+doom.el
+++ src/config/doom/+doom.el
@@ -1,15 +1,21 @@
 ;;; ~/Projects/dotfiles/src/config/doom/+doom.el -*- lexical-binding: t; -*-
 
 
-(setq doom-scratch-initial-major-mode 'org-mode)
-(setq doom-font (font-spec :family "Source Code Pro" :size 30)
+(setq doom-scratch-initial-major-mode 'org-mode
+      doom-font (font-spec :family "Source Code Pro" :size 30)
       doom-variable-pitch-font (font-spec :family "Source Code Pro")
       doom-unicode-font (font-spec :family "DejaVu Sans Mono")
       doom-big-font (font-spec :family "Source Code Pro" :size 48))
 
-(defun +doom-buffer-name (context)
-  (format "*doom:%s:%s*"
-          context
-          (if (bound-and-true-p persp-mode)
-              (safe-persp-name (get-current-persp))
-            "main")))
+
+(defun +doom-buffer-name (context &optional global &rest extra-context)
+  (let ((scope-list `("doom"
+                      ,context
+                      ,@extra-context
+                      ,(and (null global)
+                            (if (bound-and-true-p persp-mode)
+                           (safe-persp-name (get-current-persp))
+                         "main")))))
+    (concat "*" (string-join (seq-filter 'identity scope-list) ":") "*")))
+
+
