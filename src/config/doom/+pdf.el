0a6409350fcc3d5b0c96f95d24753193142d198b
Theia Ware <theia.j.ware@gmail.com>
2020-05-15 23:26:33 -0600

    PDF: Links open in vertical split and resize centers PDF page

--- src/config/doom/+pdf.el
+++ src/config/doom/+pdf.el
@@ -21,3 +21,31 @@ Those methods call `bob' and `eob', which adjust hscroll"
   (let ((hscroll (window-hscroll)))
     (apply fn args)
     (image-set-window-hscroll hscroll)))
+
+(defadvice! +pdf--org-open-links (fn &rest args)
+  "Open PDFs in a vertical split, open one if none-exists and
+don't do anything if the buffer is already displayed."
+  :around 'org-pdftools-open-pdftools
+  (let ((display-buffer-overriding-action '(+buffer--display-in-vertical-split-maybe . nil)))
+    (apply fn args)))
+
+(defun +image-center-horizontally ()
+  "Center an image (or PDF) horizontally."
+  (interactive)
+  (let* ((image (image-get-display-property))
+	 (edges (window-inside-edges))
+	 (pixel-edges (window-edges nil t t))
+	 (win-width (- (nth 2 edges) (nth 0 edges)))
+	 (img-width (ceiling (car (image-display-size image)))))
+    (image-set-window-hscroll (max 0 (/ (- img-width win-width) 2)))))
+
+(defadvice! +pdf--resize (&rest _)
+  "Center the image horizontally when resizing."
+  :after '(pdf-view-enlarge
+           pdf-view-shrink
+           pdf-view-scale-reset)
+  (+image-center-horizontally))
+
+
+(after! pdf-tools
+  (setq-default pdf-view-display-size 'fit-width))
