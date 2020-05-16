659ab4107c8649aca685f12cc1639324f1c6b776
Theia Ware <theia.j.ware@gmail.com>
2020-05-15 23:24:37 -0600

    Adding a comment to '+doom-buffer-name

--- src/config/doom/+doom.el
+++ src/config/doom/+doom.el
@@ -9,13 +9,20 @@
 
 
 (defun +doom-buffer-name (context &optional global &rest extra-context)
+  "Generates a canonical doom buffer name.
+
+With just a CONTEXT set you get: *doom:<context>*
+
+When GLOBAL is nil the workspace name or main (the default) is appended to the name, like so: *doom:<context>:main*
+
+Whatever EXTRA-CONTEXT is provided is appended after the CONTEXT: *doom:<context>:<extra>:<context>:main*
+"
   (let ((scope-list `("doom"
                       ,context
                       ,@extra-context
                       ,(and (null global)
                             (if (bound-and-true-p persp-mode)
-                           (safe-persp-name (get-current-persp))
-                         "main")))))
+                                (safe-persp-name (get-current-persp))
+                              "main")))))
     (concat "*" (string-join (seq-filter 'identity scope-list) ":") "*")))
-
 
