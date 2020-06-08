009c08fb8b652b2e50e5d5eea36916485bae712a
Theia Ware <theia.j.ware@gmail.com>
2020-06-07 22:01:16 -0600

    Deleting old setup scripts

--- setup/scripts.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-path_dir="${HOME}/.config/path"
-mkdir -p ${path_dir}
-[ -e "${path_dir}/00-scripts" ] && rm "${path_dir}/00-scripts"
-ln -s "${PWD}/src/scripts" "${path_dir}/00-scripts" 
