1172b51aefcc4df3783b0c1bd1a06ce2e2b9f5df
Theia Ware <theia.j.ware@gmail.com>
2020-06-12 12:34:04 -0600

    Unpinning fixes some things for me

--- src/config/doom/packages.el
+++ src/config/doom/packages.el
@@ -1,19 +1,25 @@
 ;; -*- no-byte-compile: t; -*-
 ;;; .doom.d/packages.el
 
-;;; Examples:
-;; (package! some-package)
-;; (package! another-package :recipe (:host github :repo "username/repo"))
-;; (package! builtin-package :disable t)
-
+;; Don't think I need these two any more:
 (package! all-the-icons-ivy)
 (package! all-the-icons-dired)
+
+
+;; I like to try things
 (package! try)
 (package! command-log-mode)
 (package! ob-sagemath)
+
+;; A curiosity
 (package! howdoyou)
+
+;; Package I made.. not sure if it's worth anything
 (package! rextract
   :recipe (:host github :repo "tylerware/rextract.el"))
 
 (package! doct)
 (package! hl-line :disable t)
+
+(unpin! ivy)
+(unpin! swiper)
