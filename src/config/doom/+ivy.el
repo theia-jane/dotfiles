a87c484f27baf78320fe9826ee046eeff44e07c0
Theia Ware <theia.j.ware@gmail.com>
2020-06-12 12:34:36 -0600

    Changing projectile related advice

--- src/config/doom/+ivy.el
+++ src/config/doom/+ivy.el
@@ -4,18 +4,8 @@
   (setq ivy-read-action-function #'ivy-hydra-read-action)
   (push '(swiper . 10) ivy-height-alist))
 
-(defadvice! +ivy--compute-extra-actions-dedup-a (results)
-  "Removes duplicates from `ivy--compute-extra-actions'. This fixes a 'bug' (?)
-where there are duplicate actions with the same key."
-  :filter-return #'ivy--compute-extra-actions
-  (setcdr results
-          (cl-delete-duplicates (cdr results)
-                         :key #'car :test #'equal
-                         :from-end t))
-  results)
 
 (defadvice! +counsel-projectile-switch-project--set-default-a (args)
   :filter-args #'counsel-projectile-switch-project
-  (list (or
-         (car args)
-         "v")))
+  (list (or (car args)
+            #'counsel-projectile-switch-project-action-vc)))
