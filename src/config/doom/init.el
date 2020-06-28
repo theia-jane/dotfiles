3d34dbb58518dab009a0008794cf617763f2fe28
Theia Ware <theia.j.ware@gmail.com>
2020-06-27 23:32:50 -0600

    Adding some modules, removing some and aligning comments

--- src/config/doom/init.el
+++ src/config/doom/init.el
@@ -19,26 +19,21 @@
        ivy                    ; a search engine for love and life
 
        :ui
-       ;;deft                 ; notational velocity for Emacs
        dashboard
        doom                   ; what makes DOOM look the way it does
        doom-dashboard         ; a nifty splash screen for Emacs
        doom-quit              ; DOOM quit-message prompts when you quit Emacs
-       ;;fill-column          ; a `fill-column' indicator
        hl-todo                ; highlight TODO/FIXME/NOTE/DEPRECATED/HACK/REVIEW
        hydra
-       ;;indent-guides        ; highlighted indent columns
        modeline               ; snazzy, Atom-inspired modeline, plus API
        nav-flash              ; blink the current line after jumping
-       ;;neotree              ; a project drawer, like NERDTree for vim
-       ophints                ; highlight the region an operation acts on
        (popup                 ; tame sudden yet inevitable temporary windows
-        +all                  ; catch all popups that start with an asterix
+        ;; +all               ; catch all popups that start with an asterix
         +defaults)            ; default popup rules
        pretty-code            ; replace bits of code with pretty symbols
        ;;tabs                 ; an tab bar for Emacs
        treemacs               ; a project drawer, like neotree but cooler
-       ;; unicode                ; extended unicode support for various languages
+       ;; unicode             ; extended unicode support for various languages
        vc-gutter              ; vcs diff in the fringe
        vi-tilde-fringe        ; fringe tildes to mark beyond EOB
        window-select          ; visually switch windows
@@ -88,7 +83,7 @@
        (lsp
         +peek)
        ;;macos                ; MacOS-specific commands
-       (magit +forge)                 ; a git porcelain for Emacs
+       (magit +forge)         ; a git porcelain for Emacs
        make                   ; run make tasks from Emacs
        pass                   ; password manager for nerds
        pdf                    ; pdf enhancements
@@ -100,7 +95,7 @@
        ;;wakatime
 
        :checkers
-       syntax
+       (syntax +childframe)
        spell
 
        :lang
@@ -108,7 +103,7 @@
        cc                     ; C/C++/Obj-C madness
        clojure                ; java with a lisp
        common-lisp            ; if you've seen one lisp, you've seen them all
-       ;; coq                  ; proofs-as-programs
+       ;; coq                 ; proofs-as-programs
        ;;crystal              ; ruby at the speed of c
        ;;csharp               ; unity, .NET, and mono shenanigans
        data                   ; config/data formats
@@ -147,7 +142,7 @@
         ;;+pomodoro           ; be fruitful with the tomato technique
         +roam
         +present)             ; using org-mode for presentations
-       ;; perl                   ; write code no one else can comprehend
+       ;; perl                ; write code no one else can comprehend
        (json +lsp)
        (php
         +lsp)                 ; perl's insecure younger brother
