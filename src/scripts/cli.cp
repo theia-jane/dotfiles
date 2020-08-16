7463ad7e12e3464fbc7b600f44a204183eea4229
Theia Ware <theia.j.ware@gmail.com>
2020-08-16 16:04:14 -0600

    Deleted unneeded scripts

--- src/scripts/cli.cp
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-fc -nl -1
-fc -nl -1 | sed 's/^\s*//' | c
--- src/scripts/repl.f
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-selected_language="$(repl --list | fzf --height 6 --prompt "Language: ")"
-repl "$selected_language"
-
-# vim: ft=sh
