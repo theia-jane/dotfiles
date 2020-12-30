f489952795ee44605fec06b68793abac613d72a9
Theia Ware <theia.j.ware@gmail.com>
2020-12-29 21:51:04 -0700

    Pulling org configuration into packages

--- tools/emacs/doom/+org.el
+++ tools/emacs/doom/+org.el
@@ -389,15 +389,7 @@
        (:prefix ("L" . "Latex")
         :desc "Preview" "p" #'org-latex-preview-buffer)))
 
-(defun org-latex-preview-buffer ()
-  (interactive)
-  (when (eq major-mode 'org-mode)
-        (org--latex-preview-region (point-min) (point-max))))
-
-(defun org-latex-preview-buffer-h ()
-  (add-hook 'after-save-hook #'org-latex-preview-buffer))
-
-(add-hook 'org-mode-hook #'org-latex-preview-buffer-h)
+(use-package my-org-extras)
 
 ;;; Babel
 (defadvice! +org-babel--resolve-tangle-path-to-dir-a (fn &optional light datum)
@@ -520,13 +512,9 @@ If :tangle-relative is
 
             ))))))
 
-(defun test/x ()
-  (goto-char 3332))
-
 (defun org-rename ()
   "TODO"
   nil)
-
 
 (defun org-get-name ()
   (let ((el (org-outer-element)))
@@ -600,34 +588,6 @@ If :tangle-relative is
   (interactive "p")
   (+org-forward-to-narrowed-heading (- arg)))
 
-(define-minor-mode org-server-manager-mode
-  "TODO"
-  :init-val nil
-  :lighter ""
-  :keymap (make-sparse-keymap))
-
-(defun org-server-manager-ssh-connect (&optional arg)
-  "Connect to the host at point and open `dired'.
-If ARG is non-nil, open `eshell' instead of `dired'."
-  (interactive "P")
-  (let* ((properties (org-entry-properties))
-         (name (alist-get "ITEM" properties nil nil #'string=))
-         (user (alist-get "SSH_USER" properties nil nil #'string=))
-         (port (alist-get "SSH_PORT" properties nil nil #'string=))
-         (host (or (alist-get "IP" properties nil nil #'string=)
-                   (alist-get "HOSTNAME" properties nil nil #'string=))))
-    (if host
-        (let ((default-directory (format "/ssh:%s%s%s:"
-                                         (if user (format "%s@" user) "")
-                                         host
-                                         (if port (format "#%s" port) ""))))
-          (message "Connecting to %s..." name)
-          (if arg
-              (eshell t)
-            (dired ".")))
-      (user-error "Not an SSH host"))))
-
+(use-package org-server-manager)
 (map! (:map org-server-manager-mode-map
        :desc "Open dired to remote" "C-c s" #'org-server-manager-ssh-connect))
-
-(add-hook 'org-servers-file-open-hook #'org-server-manager-mode)
