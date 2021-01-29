b24347411e8ce35a55fa6fd32d24aa22b6c54e61
Theia Ware <theia.j.ware@gmail.com>
2021-01-29 12:59:14 -0700

    Migrating doom +org.el to org.org

--- tools/emacs/doom/+org.el
+++ tools/emacs/doom/+org.el
@@ -1,12 +1,5 @@
 ;;; ~/Projects/dotfiles/src/config/doom/+org.el -*- lexical-binding: t; -*-
 
-(use-package! org-crypt
-  :after org
-  :defer-incrementally t
-  :config
-  (org-crypt-use-before-save-magic)
-  (setq org-tags-exclude-from-inheritance (quote ("crypt"))
-        org-crypt-key nil))
 
 (defun sp-insert-newline-inside-pair (_id action _context)
   "ID, ACTION, CONTEXT."
@@ -133,6 +126,14 @@
 
 (after! org
   (setq org-export-async-init-file (expand-file-name "org-export-init.el" (dir!)))
+
+  (use-package! org-crypt
+    :after org
+    :defer-incrementally t)
+  (use-package! ox-extra
+    :after org
+    :defer-incrementally t)
+
   (require! config-org
             config-org-latex
             config-org-babel
@@ -195,31 +196,12 @@
   (add-hook 'org-mode-hook
             #'(lambda () (yas-activate-extra-mode 'latex-mode))))
 
-(use-package! ox-extra
-  :after org
-  :defer-incrementally t
-  :config
-  (ox-extras-activate '(ignore-headlines)))
 
 (set-file-template! 'org-mode
   :when '(lambda (file)
            (file-in-directory-p file "~/homework"))
   :trigger "__hw"
   :mode 'org-mode)
-
-(defun +org/goto-next-src-block ()
-  (interactive)
-  (org-next-block 1 nil "^[ \t]*#\\+begin_src")
-  (+ui/smart-scroll-to-top))
-
-(defun +org/eval-src-block-then-next ()
-  (interactive)
-  (condition-case _err
-      (org-babel-execute-src-block)
-    (t
-     (+org/goto-next-src-block)
-     (org-babel-execute-src-block)))
-  (+org/goto-next-src-block))
 
 (defun +tw/org-ctrl-c-ret ()
   (interactive)
@@ -291,69 +273,12 @@
           (t (org-insert-todo-heading arg)))))
 
 (map! (:map org-mode-map
-       "<S-return>" 'org-ctrl-c-ctrl-c
        "<M-S-return>" '+org/M-S-Return
-       :ni "<C-S-return>" '+org/eval-src-block-then-next
-       :n "M-i" #'org-edit-special
-       :ni "M-o" #'org-cleave
        "<normal-state> <M-return>" '+tw/org-ctrl-c-ret)
       (:map evil-org-mode-map
-       :ni "<C-S-return>" '+org/eval-src-block-then-next
-       ;; evil-org text objects
-       :vo "ae" #'evil-org-an-object
-       :vo "ie" #'evil-org-inner-object
-       :vo "aE" #'evil-org-an-element
-       :vo "iE" #'evil-org-inner-element
-       :vo "ir" #'evil-org-inner-greater-element
-       :vo "ar" #'evil-org-a-greater-element
-       :vo "aR" #'evil-org-a-subtree
-       :vo "iR" #'evil-org-inner-subtree
-       :vn "zn" #'+evil:narrow-buffer)
-      (:localleader
-       :map org-mode-map
-       "n" nil ;; clear this out first (otherwise we'll get a "not a prefix key warning")
-       (:prefix ("n" . "Narrow")
-        :desc "Next heading" "]" #'+org-forward-to-narrowed-heading
-        :desc "Next heading" "[" #'+org-backward-to-narrowed-heading
-        :desc "Subtree" "s" #'org-narrow-to-subtree
-        :desc "Block" "b" #'org-narrow-to-block
-        :desc "Element" "e" #'org-narrow-element)
-       :desc "Set Name" "N" #'org-set-name
-       (:prefix ("L" . "Latex")
-        :desc "Preview" "p" #'org-latex-preview-buffer)))
-
+       :vn "zn" #'+evil:narrow-buffer))
 
 ;;; Babel
-(defadvice! +org-babel--resolve-tangle-path-to-dir-a (fn &optional light datum)
-  "Add :tangle-relative property to org babel header args.
-
-This new property will make the :tangle files relative to
-the :dir or to the value of :tangle-relative.
-
-If :tangle-relative is
-- equal to 'dir, then it uses :dir
-- a string it uses the value passed
-"
-  :around #'org-babel-get-src-block-info
-  (let ((info (funcall fn light datum)))
-    (unless light
-      (let* ((prop-alist (nth 2 info))
-             (dir (alist-get :dir prop-alist))
-             (tangle (alist-get :tangle prop-alist))
-             (tangle-relative (alist-get :tangle-relative prop-alist)))
-        (when (and dir
-                   (not (equal tangle "yes"))
-                   (not (equal tangle "no"))
-                   tangle-relative)
-          (setf (alist-get :tangle prop-alist)
-                (f-join (cond
-                         ((stringp tangle-relative) tangle-relative)
-                         ((eq tangle-relative 'dir) dir)
-                         (t ""))
-                        tangle)))))
-    info))
-
-
 (defadvice! +org-babel--buffer-arg (fn result &optional result-params info hash lang)
   "Popup results of an org src block if :buffer t is given as a header argument "
   :around  'org-babel-insert-result ;; probably a better type of advice
@@ -366,160 +291,3 @@ If :tangle-relative is
         (erase-buffer)
         (insert result)
         (+popup-buffer (current-buffer))))))
-
-(defun org-cleave ()
-  "TODO"
-  (interactive)
-  (let* ((el (org-element-at-point))
-         (el-type (car el))
-         (post-affiliated (plist-get (nth 1 el) :post-affiliated)))
-    (when (eq el-type 'src-block)
-      (let ((src-begin-line (save-excursion
-                              (goto-char post-affiliated)
-                              (buffer-substring (point) (line-end-position)))))
-        (end-of-line)
-        (insert (format "\n#+END_SRC\n\n%s\n" src-begin-line))))))
-
-(defun org-src-content-bounds (el)
-  (list
-   (save-mark-and-excursion
-     (goto-char (org-element-property :post-affiliated el))
-     (line-end-position))
-   (save-mark-and-excursion
-     (goto-char (1- (org-element-property :end el)))
-     (forward-line (- (org-element-property :post-blank el)))
-     (line-beginning-position))))
-
-(defun org-block-header (el)
-  (save-mark-and-excursion
-    (goto-char (org-element-property :post-affiliated el))
-    (buffer-substring (point) (line-end-position))))
-
-(defun org-ensure-noweb ()
-  (let* ((info (org-babel-get-src-block-info))
-         (noweb (alist-get :noweb (nth 2 info))))
-    (when (or (not noweb)
-              (equal noweb "no"))
-      (org-babel-insert-header-arg "noweb" "yes"))))
-
-(defun org-extract (beg end name)
-  "TODO"
-  (interactive
-   (list (region-beginning)
-         (region-end)
-         (read-string "Name: " (org-get-name))))
-
-  (when (and beg end)
-    (let ((el (org-element-at-point)))
-      (when (and (eq (car el) 'src-block))
-        (let* ((src-header (org-block-header el))
-               (content-bounds (org-src-content-bounds el))
-               (has-name (and name (not (equal name ""))))
-               (region-contents (buffer-substring beg end)))
-          (when (and (<= (car content-bounds) beg (cadr content-bounds))
-                     (<= (car content-bounds) end (cadr content-bounds)))
-            (setq deactivate-mark t)
-            (evil-exit-visual-state)
-            (delete-region beg end)
-            (when has-name
-              (save-excursion
-                (end-of-line)
-                (org-ensure-noweb)
-                (insert "<<" name ">>")))
-
-            (goto-char (cadr (org-src-content-bounds (org-element-at-point))))
-            (insert
-             (concat
-              (when (eq (forward-line) 1)
-                "\n")
-              "\n"
-              (when has-name
-                (concat "#+NAME: " name "\n"))
-              src-header
-              "\n"
-              region-contents)
-             )
-            (save-excursion
-              (insert "\n\n#+END_SRC\n"))
-
-            ))))))
-
-(defun org-rename ()
-  "TODO"
-  nil)
-
-(defun org-get-name ()
-  (let ((el (org-outer-element)))
-    (or (org-element-property :name el)
-        (and (equal (org-element-property :key el) "NAME")
-             (org-element-property :value el)))))
-
-(defun org-set-name (name)
-  (interactive
-   (list (read-string "Name: " (org-get-name))))
-  (let* ((el (org-outer-element))
-         (el-type (car el))
-         (existing-name (org-get-name)))
-    (when (and el-type
-               (or (not (memq el-type '(headline keyword)))
-                   (and (eq el-type 'keyword)
-                        (equal (org-element-property :key el) "NAME")))
-
-               (save-excursion
-                 (goto-char (org-element-property :begin el))
-
-                 (unless existing-name
-                   (beginning-of-line)
-                   (save-excursion
-                     (insert "#+NAME:\n")))
-
-                 (if (or (null name) (equal name ""))
-                     (delete-region (point) (1+ (line-end-position)))
-                   (save-match-data
-                     (re-search-forward (rx "#+NAME:"))
-                     (delete-region (match-end 0) (line-end-position)))
-                   (end-of-line)
-                   (insert (concat " " name))))))))
-
-(defun org-parent-property (property context)
-  (when-let ((parent (org-element-property :parent context)))
-    (org-element-property property parent)))
-
-(defun org-inherited-property (property context)
-  (or (org-element-property property context)
-      (org-parent-property property context)))
-
-(defun org-top-level-property (property context)
-  (or (org-parent-property property context)
-      (org-element-property property context)))
-
-(defun org-outer-element (&optional context)
-  (let* ((el (or context (org-element-at-point)))
-         (parent (org-element-property :parent el)))
-    (or parent el)))
-
-(defun +org-forward-to-narrowed-heading (arg)
-  (interactive "p")
-  (when (buffer-narrowed-p)
-    (goto-char (point-min))
-    (org-forward-heading-same-level arg)
-    (widen))
-  (org-forward-heading-same-level p)
-  (org-narrow-to-subtree))
-
-(map!
- :nivo "M-i" doom-localleader-key)
-
-(defun doom-send-localleader ()
-  (interactive)
-  (setq prefix-arg current-prefix-arg)
-  (setq unread-command-events (append (listify-key-sequence (kbd "SPC")) '(?m))))
-
-
-(defun +org-backward-to-narrowed-heading (arg)
-  (interactive "p")
-  (+org-forward-to-narrowed-heading (- arg)))
-
-(use-package org-server-manager)
-(map! (:map org-server-manager-mode-map
-       :desc "Open dired to remote" "C-c s" #'org-server-manager-ssh-connect))
