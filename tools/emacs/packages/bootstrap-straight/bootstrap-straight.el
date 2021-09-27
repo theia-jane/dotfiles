748b6035602c1576e4e24307b033e2df19abc7a7
Theia Ware <theia.j.ware@gmail.com>
2021-09-27 16:24:34 -0600

    Getting rid of elpm and some associated refactor

---
+++ tools/emacs/packages/bootstrap-straight/bootstrap-straight.el
@@ -0,0 +1,25 @@
+;;; bootstrap-straight.el --- Bootstrap straight -*- lexical-binding: t; -*-
+;;
+;; This file is not part of GNU Emacs.
+;;
+;;  Bootstrap straight
+;;
+;;; Code:
+
+(defun bootstrap-straight ()
+  "Bootstrap straight.el."
+  (defvar bootstrap-version)
+  (let ((bootstrap-file
+         (expand-file-name "straight/repos/straight.el/bootstrap.el" user-emacs-directory))
+        (bootstrap-version 5))
+    (unless (file-exists-p bootstrap-file)
+      (with-current-buffer
+          (url-retrieve-synchronously
+           "https://raw.githubusercontent.com/raxod502/straight.el/develop/install.el"
+           'silent 'inhibit-cookies)
+        (goto-char (point-max))
+        (eval-print-last-sexp)))
+    (load bootstrap-file nil 'nomessage)))
+
+(provide 'bootstrap-straight)
+;;; bootstrap-straight.el ends here
