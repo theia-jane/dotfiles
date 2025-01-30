93a495833667b1351d6737cfcd369cd19ad1fd8a
Theia Ware <theia.j.ware@gmail.com>
2025-01-29 17:44:17 -0800

    Split up personal-lib.
    Only want the base version available with doom. Otherwise can use
    the extended package

---
+++ personal-lib-x.el
@@ -0,0 +1,105 @@
+;;; personal-lib-x.el --- Description -*- lexical-binding: t; -*-
+;;
+;; Copyright (C) 2025 Theia Ware
+;;
+;; Author: Theia Ware <mail@theia-jane.dev>
+;; Maintainer: Theia Ware <mail@theia-jane.dev>
+;; Created: January 29, 2025
+;; Modified: January 29, 2025
+;; Version: 0.0.1
+;; Package-Requires: ((emacs "24.3"))
+;;
+;; This file is not part of GNU Emacs.
+;;
+;;; Commentary:
+;;
+;;  Description
+;;
+;;; Code:
+
+(defmacro defer-until! (condition &rest body)
+  "Run BODY when CONDITION is true (checks on `after-load-functions'). Meant to
+serve as a predicated alternative to `after!'.
+
+Taken from doom-emacs."
+  (declare (indent defun) (debug t))
+  `(if ,condition
+       (progn ,@body)
+     ,(let ((fn (intern (format "--delay-form-%s-h" (sxhash (cons condition body))))))
+        `(progn
+           (fset ',fn (lambda (&rest args)
+                        (when ,(or condition t)
+                          (remove-hook 'after-load-functions #',fn)
+                          (unintern ',fn nil)
+                          (ignore args)
+                          ,@body)))
+           (put ',fn 'permanent-local-hook t)
+           (add-hook 'after-load-functions #',fn)))))
+
+
+(defmacro setq-hook! (hooks &rest var-vals)
+  "Sets buffer-local variables on HOOKS.
+
+\(fn HOOKS &rest [SYM VAL]...)"
+  (declare (indent 1))
+  (macroexp-progn
+   (cl-loop for (var val hook fn) in (setq-hook-fns hooks var-vals)
+            collect `(defun ,fn (&rest _)
+                       ,(format "%s = %s" var (pp-to-string val))
+                       (setq-local ,var ,val))
+            collect `(remove-hook ',hook #',fn) ; ensure set order
+            collect `(add-hook ',hook #',fn))))
+
+(defmacro unsetq-hook! (hooks &rest vars)
+  "Unbind setq hooks on HOOKS for VARS.
+
+\(fn HOOKS &rest [SYM VAL]...)"
+  (declare (indent 1))
+  (macroexp-progn
+   (cl-loop for (_var _val hook fn)
+            in (setq-hook-fns hooks vars 'singles)
+            collect `(remove-hook ',hook #',fn))))
+
+
+(defmacro defadvice! (symbol arglist &optional docstring &rest body)
+  "Define an advice called SYMBOL and add it to PLACES.
+
+ARGLIST is as in `defun'. WHERE is a keyword as passed to `advice-add', and
+PLACE is the function to which to add the advice, like in `advice-add'.
+DOCSTRING and BODY are as in `defun'.
+
+\(fn SYMBOL ARGLIST &optional DOCSTRING &rest [WHERE PLACES...] BODY\)"
+  (declare (doc-string 3) (indent defun))
+  (unless (stringp docstring)
+    (push docstring body)
+    (setq docstring nil))
+  (let (where-alist)
+    (while (keywordp (car body))
+      (push `(cons ,(pop body) (enlist ,(pop body)))
+            where-alist))
+    `(progn
+       (defun ,symbol ,arglist ,docstring ,@body)
+       (dolist (targets (list ,@(nreverse where-alist)))
+         (dolist (target (cdr targets))
+           (advice-add target (car targets) #',symbol))))))
+
+(defmacro undefadvice! (symbol _arglist &optional docstring &rest body)
+  "Undefine an advice called SYMBOL.
+
+This has the same signature as `defadvice!' an exists as an easy undefiner when
+testing advice (when combined with `rotate-text').
+
+\(fn SYMBOL ARGLIST &optional DOCSTRING &rest [WHERE PLACES...] BODY\)"
+  (declare (doc-string 3) (indent defun))
+  (let (where-alist)
+    (unless (stringp docstring)
+      (push docstring body))
+    (while (keywordp (car body))
+      (push `(cons ,(pop body) (enlist ,(pop body)))
+            where-alist))
+    `(dolist (targets (list ,@(nreverse where-alist)))
+       (dolist (target (cdr targets))
+         (advice-remove target #',symbol)))))
+
+(provide 'personal-lib-x)
+;;; personal-lib-x.el ends here
--- tools/emacs/packages/personal-lib/personal-lib.el
+++ tools/emacs/packages/personal-lib/personal-lib.el
@@ -28,7 +28,6 @@ Just leverages `use-package'."
 
 Alias of `after!' that is a little more descriptive at times.")
 
-
 (defmacro before! (package &rest body)
   "Do BODY before PACKAGE has been loaded.
 
@@ -42,24 +41,6 @@ Just leverages `use-package'."
   'before!
   "Initialize a package (settings to be set before it's loaded)")
 
-(defmacro defer-until! (condition &rest body)
-  "Run BODY when CONDITION is true (checks on `after-load-functions'). Meant to
-serve as a predicated alternative to `after!'.
-
-Taken from doom-emacs."
-  (declare (indent defun) (debug t))
-  `(if ,condition
-       (progn ,@body)
-     ,(let ((fn (intern (format "--delay-form-%s-h" (sxhash (cons condition body))))))
-        `(progn
-           (fset ',fn (lambda (&rest args)
-                        (when ,(or condition t)
-                          (remove-hook 'after-load-functions #',fn)
-                          (unintern ',fn nil)
-                          (ignore args)
-                          ,@body)))
-           (put ',fn 'permanent-local-hook t)
-           (add-hook 'after-load-functions #',fn)))))
 (defun resolve-hook-forms (hooks)
   "Converts a list of modes into a list of hook symbols.
 
@@ -105,144 +86,6 @@ list is returned as-is."
            (advice-add on :before fn)
          (add-hook on fn)))))
 
-(defmacro add-hook! (hooks &rest rest)
-  "A convenience macro for adding N functions to M hooks.
-
-This macro accepts, in order:
-
-  1. The mode(s) or hook(s) to add to. This is either an unquoted mode, an
-     unquoted list of modes, a quoted hook variable or a quoted list of hook
-     variables.
-  2. Optional properties :local and/or :append, which will make the hook
-     buffer-local or append to the list of hooks (respectively),
-  3. The function(s) to be added: this can be one function, a quoted list
-     thereof, a list of `defun's, or body forms (implicitly wrapped in a
-     lambda).
-
-\(fn HOOKS [:append :local] FUNCTIONS)"
-  (declare (indent (lambda (indent-point state)
-                     (goto-char indent-point)
-                     (when (looking-at-p "\\s-*(")
-                       (lisp-indent-defform state indent-point))))
-           (debug t))
-  (let* ((hook-forms (resolve-hook-forms hooks))
-         (func-forms ())
-         (defn-forms ())
-         append-p
-         local-p
-         remove-p
-         forms)
-    (while (keywordp (car rest))
-      (pcase (pop rest)
-        (:append (setq append-p t))
-        (:local  (setq local-p t))
-        (:remove (setq remove-p t))))
-    (let ((first (car-safe (car rest))))
-      (cond ((null first)
-             (setq func-forms rest))
-
-            ((eq first 'defun)
-             (setq func-forms (mapcar #'cadr rest)
-                   defn-forms rest))
-
-            ((memq first '(quote function))
-             (setq func-forms
-                   (if (cdr rest)
-                       (mapcar #'unquote rest)
-                     (enlist (unquote (car rest))))))
-
-            ((setq func-forms (list `(lambda (&rest _) ,@rest)))))
-      (dolist (hook hook-forms)
-        (dolist (func func-forms)
-          (push (if remove-p
-                    `(remove-hook ',hook #',func ,local-p)
-                  `(add-hook ',hook #',func ,append-p ,local-p))
-                forms)))
-      (macroexp-progn
-       (append defn-forms
-               (if append-p
-                   (nreverse forms)
-                 forms))))))
-
-(defmacro remove-hook! (hooks &rest rest)
-  "A convenience macro for removing N functions from M hooks.
-
-Takes the same arguments as `add-hook!'.
-
-If N and M = 1, there's no benefit to using this macro over `remove-hook'.
-
-\(fn HOOKS [:append :local] FUNCTIONS)"
-  (declare (indent defun) (debug t))
-  `(add-hook! ,hooks :remove ,@rest))
-
-
-(defmacro setq-hook! (hooks &rest var-vals)
-  "Sets buffer-local variables on HOOKS.
-
-\(fn HOOKS &rest [SYM VAL]...)"
-  (declare (indent 1))
-  (macroexp-progn
-   (cl-loop for (var val hook fn) in (setq-hook-fns hooks var-vals)
-            collect `(defun ,fn (&rest _)
-                       ,(format "%s = %s" var (pp-to-string val))
-                       (setq-local ,var ,val))
-            collect `(remove-hook ',hook #',fn) ; ensure set order
-            collect `(add-hook ',hook #',fn))))
-
-(defmacro unsetq-hook! (hooks &rest vars)
-  "Unbind setq hooks on HOOKS for VARS.
-
-\(fn HOOKS &rest [SYM VAL]...)"
-  (declare (indent 1))
-  (macroexp-progn
-   (cl-loop for (_var _val hook fn)
-            in (setq-hook-fns hooks vars 'singles)
-            collect `(remove-hook ',hook #',fn))))
-(defmacro defadvice! (symbol arglist &optional docstring &rest body)
-  "Define an advice called SYMBOL and add it to PLACES.
-
-ARGLIST is as in `defun'. WHERE is a keyword as passed to `advice-add', and
-PLACE is the function to which to add the advice, like in `advice-add'.
-DOCSTRING and BODY are as in `defun'.
-
-\(fn SYMBOL ARGLIST &optional DOCSTRING &rest [WHERE PLACES...] BODY\)"
-  (declare (doc-string 3) (indent defun))
-  (unless (stringp docstring)
-    (push docstring body)
-    (setq docstring nil))
-  (let (where-alist)
-    (while (keywordp (car body))
-      (push `(cons ,(pop body) (enlist ,(pop body)))
-            where-alist))
-    `(progn
-       (defun ,symbol ,arglist ,docstring ,@body)
-       (dolist (targets (list ,@(nreverse where-alist)))
-         (dolist (target (cdr targets))
-           (advice-add target (car targets) #',symbol))))))
-
-(defmacro undefadvice! (symbol _arglist &optional docstring &rest body)
-  "Undefine an advice called SYMBOL.
-
-This has the same signature as `defadvice!' an exists as an easy undefiner when
-testing advice (when combined with `rotate-text').
-
-\(fn SYMBOL ARGLIST &optional DOCSTRING &rest [WHERE PLACES...] BODY\)"
-  (declare (doc-string 3) (indent defun))
-  (let (where-alist)
-    (unless (stringp docstring)
-      (push docstring body))
-    (while (keywordp (car body))
-      (push `(cons ,(pop body) (enlist ,(pop body)))
-            where-alist))
-    `(dolist (targets (list ,@(nreverse where-alist)))
-       (dolist (target (cdr targets))
-         (advice-remove target #',symbol)))))
-
-(defmacro prependq! (sym &rest lists)
-  "Prepend LISTS to SYM in place.
-
-From doom-emacs."
-  `(setq ,sym (append ,@lists ,sym)))
 
 (defmacro with-directory (directory &rest body)
   `(let ((default-directory ,directory))
