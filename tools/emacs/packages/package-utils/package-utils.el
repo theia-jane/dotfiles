d45647ad22f0e81a83e70a27b1e639a1326a4be9
Theia Ware <theia.j.ware@gmail.com>
2025-07-11 18:18:36 -0700

    refactor(emacs/package-utils): abandon global-packages commands
    This functionality is over-engineered and requires me to have
    the async package as a submodule. I don't like that, so I'm simplifying
    this. This breaks the global packages (resolved in coming commits)

--- tools/emacs/packages/package-utils/package-utils.el
+++ tools/emacs/packages/package-utils/package-utils.el
@@ -5,7 +5,6 @@
 ;;; Code:
 (defun update-repo-load-path (&optional config-dir)
   (interactive)
-  (message "root %s" config-dir)
   (let ((root (or config-dir (substitute-env-vars "$dot"))))
     (unless (string= "" root)
 	(dolist (d (cons root
@@ -15,46 +14,8 @@
 (update-repo-load-path
   (file-name-concat (file-name-directory load-file-name) ".."))
 
+(require 'bootstrap-straight)
 (require 'variable-utils)
-(require 'bootstrap-straight) 
-(require 'process-helpers) 
-(require 'info)
-(require 'async)
-
-(defconst global-packages--running-env-var "EMACS_GLOBAL_PACKAGES_RUNNING")
-(defconst global-packages--running-p (equal (getenv global-packages--running-env-var) "TRUE"))
-(defvar global-packages-dir
-  (expand-file-name "~/.local/emacs/site-lisp/global-packages/"))
-
-(defmacro global-packages--intern (&rest body) 
-  `(progn 
-     (setq user-emacs-directory ,global-packages-dir
-           global-packages--in-session-p t)
-     (require 'bootstrap-straight) 
-     (require 'info) 
-     (eject-differences ,global-packages-save-variables
-                        (bootstrap-straight)
-                        ,@body)))
-
-(defconst global-packages-save-variables '(load-path Info-directory-list))
-
-(defmacro global-packages (&rest body)
-  (unless global-packages--running-p
-    `(progn 
-       ;; (write-region "Global packages called...\n" nil "/tmp/test-log" 'append)
-       (setenv global-packages--running-env-var "TRUE")
-       (let ((p (async-start
-                 (lambda () (global-packages--intern ,@body))
-                 #'add-all-to-lists)))
-         (wait-for-process p)
-         (setenv global-packages--running-env-var "FALSE")))))
-
-(defmacro global-packages! (&rest recipes)
-  `(global-packages
-    ,@(mapcar
-       (lambda (recipe)
-         `(straight-use-package ',recipe))
-       recipes)))
 
 (defmacro require! (&rest requires)
   `(progn
@@ -69,7 +30,6 @@
         (lambda (recipe)
           `(straight-use-package ',recipe))
         recipes)))
-
 
 
 (provide 'package-utils)
